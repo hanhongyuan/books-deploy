aee6e5f4fbf230b6aec330b32a13cb609c846845
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-09-16 11:42:19 +0100

    Fix travis script build

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 SWARM_ID=$(uuidgen | cut -c 1-6)
 echo "======>>>>>> Starting swarm with ID $SWARM_ID ..."
 
-swarm-start.sh $SWARM_ID \
+./swarm-start.sh $SWARM_ID \
     && echo "======>>>>>> Swarm with ID $SWARM_ID has been started" \
     && sh -x ./swarm-deploy.sh $SWARM_ID \
     && echo "======>>>>>> Books has been deployed" \
